2d86f8c36d66bf5a12075ab20cbd0d90092058f1
Stepan Kalinin <kalinin.sa@phystech.edu>
2017-11-27 19:14:27 +0300

    Unused import fix

--- src/main/java/ru/mipt/java2017/hw3/SheetAdder.java
+++ src/main/java/ru/mipt/java2017/hw3/SheetAdder.java
@@ -3,7 +3,6 @@ package ru.mipt.java2017.hw3;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.Executable;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
